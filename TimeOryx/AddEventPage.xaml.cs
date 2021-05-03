35fe91d6c9d0c970682439a585f73fdfcce79d97
Sovego <egor.sofronov03@gmail.com>
2021-05-03 21:52:20 +0700

    adding tasks, displaying a list of tasks (only shows the last xs why)

---
+++ TimeOryx/AddEventPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeOryx
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddEventPage : ContentPage
+    {
+        private string _folderpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+       // private List<DoList> eventDoLists = new List<DoList>();
+       private string[] _tempdolist =new string[6];
+
+       public AddEventPage()
+        {
+            InitializeComponent();
+        }
+       private void Save_OnClicked(object sender, EventArgs e)
+        {
+            StreamWriter teStreamWriter;
+            _tempdolist[0] = Entry.Text;
+            _tempdolist[1] = Editor.Text;
+            _tempdolist[2] = Convert.ToString(DatePicker.Date);
+            _tempdolist[3] = Convert.ToString(TimePicker.Time);
+            if (Box.IsChecked)
+            {
+                _tempdolist[4] = Convert.ToString(DatePickerEnd.Date);
+            }
+            //eventDoLists.Add(tempDoList);
+            _tempdolist[5] = "///////////";
+            teStreamWriter = File.AppendText(Path.Combine(_folderpath, "Todo.dat"));
+            foreach (var i in _tempdolist)
+            {
+                teStreamWriter.WriteLine(i);
+            }
+            teStreamWriter.Close();
+            Navigation.PopModalAsync();
+        }
+
+        private void CBox_OnCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (Box.IsChecked)
+            {
+                DatePickerEnd.IsVisible = true;
+            }
+            else
+            {
+                DatePickerEnd.IsVisible = false;
+            }
+        }
+        private void Cancel_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ TimeOryx/TodoListPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeOryx
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodoListPage : ContentPage
+    {
+        public ObservableCollection<DoList> ToDoLists { get; set; }
+        //public ObservableCollection<DoList> ToDoLists => toDoLists;
+        private DoList _tempDoList = new DoList();
+        private string _folderpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        private string[] _tempStrings = new string[5];
+        public TodoListPage()
+        {
+            InitializeComponent();
+            ToDoLists = new ObservableCollection<DoList>();
+            int i=0;
+            if (File.Exists(Path.Combine(_folderpath,"Todo.dat")))
+            {
+                var temp = File.OpenText(Path.Combine(_folderpath, "Todo.dat"));
+                while (!temp.EndOfStream)
+                {
+                    var temstr = temp.ReadLine();
+                    if (temstr == "///////////")
+                    {
+                        _tempDoList.Name = _tempStrings[0];
+                        _tempDoList.Description = _tempStrings[1];
+                        _tempDoList.Date = _tempStrings[2];
+                        _tempDoList.Time = _tempStrings[3];
+                        _tempDoList.DateEnd = _tempStrings[4];
+                        ToDoLists.Add(_tempDoList);
+                        i = 0;
+                        continue;
+                    }
+                    _tempStrings[i] = temstr;
+                    i++;
+                }
+                temp.Close();
+            }
+            this.BindingContext = this;
+        }
+
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    ToDoLists = new ObservableCollection<DoList>();
+        //    int i=0;
+        //    if (File.Exists(Path.Combine(_folderpath,"Todo.dat")))
+        //    {
+        //        var temp = File.OpenText(Path.Combine(_folderpath, "Todo.dat"));
+        //        while (!temp.EndOfStream)
+        //        {
+        //            var temstr = temp.ReadLine();
+        //            if (temstr == "///////////")
+        //            {
+        //                _tempDoList.Name = _tempStrings[0];
+        //                _tempDoList.Description = _tempStrings[1];
+        //                _tempDoList.Date = _tempStrings[2];
+        //                _tempDoList.Time = _tempStrings[3];
+        //                _tempDoList.DateEnd = _tempStrings[4];
+        //                ToDoLists.Add(_tempDoList);
+        //                continue;
+        //            }
+
+        //            _tempStrings[i] = temstr;
+        //            i++;
+        //            if (i == 5)
+        //            {
+        //                i = 0;
+        //            }
+        //        }
+        //        temp.Close();
+        //    }
+        //    this.BindingContext = this;
+        //    UpdateChildrenLayout();
+        //}
+
+        
+
+        private void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new AddEventPage());
+        }
+    }
+}
